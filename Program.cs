41192eead66c248df3ce0a10615bc9bda56f9a4d
Ad-Astraa <lrnddiniz@gmail.com>
2021-06-05 17:43:10 -0300

    Finalização da aplicação e adição de novos tratamentos de exceção

--- Program.cs
+++ Program.cs
@@ -24,13 +24,24 @@ namespace Rev
                         //var nota = decimal.Parse(Console.ReadLine()); 
                         //aluno.Nota = decimal.Parse(Console.ReadLine());
 
+
+                        
                         if(decimal.TryParse(Console.ReadLine(), out decimal nota))
                         {
-                            aluno.Nota = nota;
+                            if(nota <= 10)
+                            {
+                                aluno.Nota = nota;
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Valor da nota deve ser menor ou igual a 10!\n\n\n\n");
+                            }
+
                         }
+                            
                         else
                         {
-                            throw new ArgumentException("Valor da nota deve ser decimal!");
+                            throw new ArgumentException("Valor da nota deve ser decimal!\n\n\n\n");
                         }
                         alunos[indiceAluno] = aluno;
                         indiceAluno++;
@@ -47,16 +58,45 @@ namespace Rev
                         // Listar Alunos
                         break;
                     case "3":
-                        var notaTotal = 0;
+                        decimal notaTotal = 0;
                         var nrAlunos = 0;
                         for(int i = 0; i < alunos.Length; i++)
                         {
-                            if(!string.IsNullOrEmpty(alunos.Nome))
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
                             {
-                                Console.WriteLine($"Aluno: {a.Nome}\nNota: {a.Nota}");
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
                             }
-
                         }
+
+                        var mediaGeral = notaTotal/nrAlunos;
+                        
+
+                        Conceito conceitoGeral;
+
+                        if(mediaGeral <= 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if(mediaGeral <= 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if(mediaGeral <= 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if(mediaGeral <= 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"Media Geral: {mediaGeral} - Conceito: {conceitoGeral}");
+
 
                         //TODO: Calcular Media Geral
                         break;
@@ -69,7 +109,7 @@ namespace Rev
         }
         private static string ObterOpcaoUsuario()
         {
-            Console.WriteLine("\nInforme a opção desejada: \n1 - Inserir novo aluno\n2 - Listar alunos\n3 - Calcular média geral\nX - Sair\n............................................");
+            Console.WriteLine("\n............................................\nInforme a opção desejada: \n1 - Inserir novo aluno\n2 - Listar alunos\n3 - Calcular média geral\nX - Sair\n............................................");
             string opcaoUsuario = Console.ReadLine();
             Console.WriteLine();
             return opcaoUsuario;
